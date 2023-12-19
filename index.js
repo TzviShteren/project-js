ae709fb199bfea7de5af656eef0993441f9bcf6e
tzevi stern <z02072000@gmail.com>
2023-12-19 21:53:17 +0200

    First real update

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('111');
+// function Title update
+function updateTitle(title) {
+    document.querySelector('.Sort.title').textContent = title;
+}
+
+
+/*
+------ local Storage ------
+const data = {
+    name: 'david',
+    age: 20,
+}
+
+localStorage.setItem('favourites', JSON.stringify(data));
+
+const storageData = JSON.parse(localStorage.getItem('favourites'));
+---------------------------
+*/
