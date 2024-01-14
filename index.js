7b4548c78a0f2e1c3bf86f4cafc9524ccb75837a
tzevi stern <z02072000@gmail.com>
2024-01-14 17:41:58 +0200

    for now

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-//import (dir);
-
-
 // function Title update
-function updateTitle(title) {
-    document.querySelector('.Sort.title').textContent = title;
-}
+let button_Now_playing = document.querySelector('.Now_playing-button');
+let button_of_favourites = document.querySelector('.favourites-button');
+let button_select_Genres = document.querySelector('.dropdown-menu');
+console.log(button_select_Genres.value);
+
+button_Now_playing.addEventListener('click',(_) =>{
+    document.querySelector('.Sort-title').value = 'Now Playing';
+})
+
+button_of_favourites.addEventListener('click',(_) =>{
+    document.querySelector('.Sort-title').value = 'favourites';
+})
+
+button_select_Genres.addEventListener('change', (_) => {
+    console.log(button_select_Genres);
+    sortTitle.textContent = button_select_Genres.options[button_select_Genres.selectedIndex].text;
+});
+
+
+
+//<-- Modal -->
+const myModal = new bootstrap.Modal('#myModal', {
+    keyboard: false
+  })
 
 
 /*
